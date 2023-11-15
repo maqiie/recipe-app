c3db853c06457a2c96b545ed1ed3d97639fe9cdc
maqiie <99504010+maqiie@users.noreply.github.com>
2023-11-15 19:28:20 +0300

    initial

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <section>
+                    <nav>
+                        <ul className="menu"></ul>
+                    </nav>
+                </section>
+                
+            </div>
+        );
+    }
+}
+
+export default Home;
